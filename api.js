2adf7dca3f8179e059e5c8e1fd590838976876dd
GmanMcman <graciousadowei@gmail.com>
2020-08-16 03:01:43 +0100

    Added back sprites, sorted sprite arrays and extra comments

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
 // Stores path to RESTful API in variable
 let pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-// Returns a promise containing resource in JSON format
+// Returns a promise containing resource parsed in JSON format
 // Default argument = pokeApiUrl
-const getPokeApi = async (url=pokeApiUrl) => {
+const getPokeApi = async (path = pokeApiUrl) => {
 	try {
-		const response = await fetch(url);
+		const response = await fetch(path);
 		const pokeApi = await response.json();
 		return pokeApi;
-		// Handles errors
+		// Handles potential errors
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 	}
@@ -17,11 +17,13 @@ const getPokeApi = async (url=pokeApiUrl) => {
 // Returns a promise containing array of names
 const getPokeNames = async () => {
 	try {
+		//
 		const api = await getPokeApi();
 		// Iterates over results key and pushes name to array
-		let pokeNames = [];
-		api.results.forEach(pokemon => pokeNames.push(pokemon.name));
+		const pokeNames = [];
+		api.results.forEach((pokemon) => pokeNames.push(pokemon.name));
 		return pokeNames;
+		// Handles potential errors
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 	}
@@ -29,19 +31,40 @@ const getPokeNames = async () => {
 
 // Returns a promise containing array of urls
 const getPokeSprites = async () => {
-	const api = await getPokeApi();
-	//
-	const pokeUrls = [];
-	api.results.forEach(pokemon => pokeUrls.push(pokemon.url))
-	const pokeSprites = [];
-	pokeUrls.forEach(async (url) => {
-		const pokeObject = await getPokeApi(url)
-		const sprite = await pokeObject.sprites.front_default
-		pokeSprites.push(sprite)
-	})
-	return pokeSprites
+	try {
+		//
+		const api = await getPokeApi();
+		// Iterates over results key and pushes each pokemon url to array
+		const pokeUrls = [];
+		api.results.forEach((pokemon) => pokeUrls.push(pokemon.url));
+		// Creates empty arrays to contain sprites
+		const frontSpriteArray = [];
+		const backSpriteArray = [];
+		const pokeSprites = {
+			front: "",
+			back: "",
+		};
+		pokeUrls.forEach(async (path) => {
+			// Reuses getPokeApi function to fetch and parse pokeUrls in JSON format
+			const pokeObject = await getPokeApi(path);
+			const frontSprite = await pokeObject.sprites.front_default;
+			const backSprite = await pokeObject.sprites.back_default;
+			// Pushes sprites to respective arrays
+			frontSpriteArray.push(frontSprite);
+			backSpriteArray.push(backSprite);
+			// Sorts arrays in ascending numerical order
+			const sortedFrontSpriteArray = frontSpriteArray.sort((a, b) => a - b)
+			const sortedBackSpriteArray = backSpriteArray.sort((a, b) => a - b)
+			// Assigns sorted arrays as values to pokeSprites object keys
+			pokeSprites.front = sortedFrontSpriteArray;
+			pokeSprites.back = sortedBackSpriteArray;
+		});
+		return pokeSprites;
+		// Handles potential errors
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+	}
 };
-
 console.log(getPokeApi())
 console.log(getPokeNames())
 console.log(getPokeSprites())
