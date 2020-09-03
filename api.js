63c3912cca7a92d45fafe80d298c45ca8131a3ed
GmanMcman <graciousadowei@gmail.com>
2020-09-04 00:06:27 +0100

    basic pokedex

--- api.js
+++ api.js
@@ -2,69 +2,14 @@
 let pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 // Returns a promise containing resource parsed in JSON format
-// Default argument = pokeApiUrl
 const getPokeApi = async (path = pokeApiUrl) => {
 	try {
 		const response = await fetch(path);
 		const pokeApi = await response.json();
+		// console.log(path);
 		return pokeApi;
 		// Handles potential errors
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 	}
 };
-
-// Returns a promise containing array of names
-const getPokeNames = async () => {
-	try {
-		//
-		const api = await getPokeApi();
-		// Iterates over results key and pushes name to array
-		const pokeNames = [];
-		api.results.forEach((pokemon) => pokeNames.push(pokemon.name));
-		return pokeNames;
-		// Handles potential errors
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-	}
-};
-
-// Returns a promise containing array of urls
-const getPokeSprites = async () => {
-	try {
-		//
-		const api = await getPokeApi();
-		// Iterates over results key and pushes each pokemon url to array
-		const pokeUrls = [];
-		api.results.forEach((pokemon) => pokeUrls.push(pokemon.url));
-		// Creates empty arrays to contain sprites
-		const frontSpriteArray = [];
-		const backSpriteArray = [];
-		const pokeSprites = {
-			front: "",
-			back: "",
-		};
-		pokeUrls.forEach(async (path) => {
-			// Reuses getPokeApi function to fetch and parse pokeUrls in JSON format
-			const pokeObject = await getPokeApi(path);
-			const frontSprite = await pokeObject.sprites.front_default;
-			const backSprite = await pokeObject.sprites.back_default;
-			// Pushes sprites to respective arrays
-			frontSpriteArray.push(frontSprite);
-			backSpriteArray.push(backSprite);
-			// Sorts arrays in ascending numerical order
-			const sortedFrontSpriteArray = frontSpriteArray.sort((a, b) => a - b)
-			const sortedBackSpriteArray = backSpriteArray.sort((a, b) => a - b)
-			// Assigns sorted arrays as values to pokeSprites object keys
-			pokeSprites.front = sortedFrontSpriteArray;
-			pokeSprites.back = sortedBackSpriteArray;
-		});
-		return pokeSprites;
-		// Handles potential errors
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-	}
-};
-console.log(getPokeApi())
-console.log(getPokeNames())
-console.log(getPokeSprites())
--- dom.js
+++ dom.js
@@ -1,35 +1,64 @@
-const container = document.querySelector("#container")
-const pokedex = document.querySelector("#pokedex-display")
+// Hoisted function calls
+displayPokedex();
 
-// Creates parent div containing sprite and name elements
-const displayPokedex = () => {
+document.addEventListener("DOMContentLoaded", () => {
+	setPokeContent();
+	viewHandler();
+});
+
+// FUNCTIONS
+// Generates HTML to contain Pokemon sprites and names
+function displayPokedex() {
+	const main = document.querySelector("main");
+	const pokedex = document.querySelector("#pokedex-display");
+	main.appendChild(pokedex);
+
+	// Runs 20 times (from i = 0 to 19)
 	for (i = 0; i < 20; i++) {
-		// Creates parent div
+		// Creates parent to contain all children
 		const pokeDiv = document.createElement("div");
 		pokeDiv.setAttribute("class", "poke-div");
 
-		// Creates child div to contain sprite
-		const createImgDivs = () => {
-			const imgDiv = document.createElement("div");
-			imgDiv.setAttribute("class", "img-div");
-			imgDiv.setAttribute("id", `img-div-${i + 1}`);
-			pokeDiv.appendChild(imgDiv);
-		};
+		// Hoisted function calls
+		htmlGenerator("img");
+		htmlGenerator("p");
 
-		// Creates child div to contain name
-		const createNameDivs = () => {
-			const nameDiv = document.createElement("div");
-			nameDiv.setAttribute("class", "name-div");
-			nameDiv.setAttribute("id", `name-div-${i + 1}`);
-			pokeDiv.appendChild(nameDiv);
-		};
+		// Creates children div to contain Pokemon sprites and names
+		function htmlGenerator(tag) {
+			element = document.createElement(tag);
+			const elementDiv = document.createElement("div");
+			elementDiv.setAttribute("class", `${tag}-div`);
+			elementDiv.setAttribute("id", `${tag}-div-${i + 1}`);
+			elementDiv.appendChild(element);
+			pokeDiv.appendChild(elementDiv);
+		}
 
-        // Calls functions
-		createImgDivs();
-		createNameDivs();
+		// Calls functions
 		pokedex.appendChild(pokeDiv);
-    }
-    container.appendChild(pokedex)
-};
+	}
+}
 
-displayPokedex()
+// Fetch resources from API and display content
+async function setPokeContent() {
+	try {
+		for (i = 0; i < 20; i++) {
+			// Fetch resource
+			const pokeUrl = await getPokeApi(`${pokeApiUrl}${i + 1}`);
+			// Grabs HTML elements
+			const img = document.querySelector(`#img-div-${i + 1}`).firstChild;
+			const p = document.querySelector(`#p-div-${i + 1}`).firstChild;
+			// Set contents
+			img.src = pokeUrl.sprites.front_default;
+			// Click functionality
+			img.addEventListener("mouseenter", () => {
+					img.src = pokeUrl.sprites.back_default;
+			});
+			img.addEventListener("mouseleave", () => {
+				img.src = pokeUrl.sprites.front_default;
+			});	
+			p.innerHTML = pokeUrl.name;
+		}
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+	}
+}
